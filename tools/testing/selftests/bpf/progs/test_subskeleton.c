60cacfb851f92f6bd19bec758d2763147f8c7641
Delyan Kratunov <delyank@fb.com>
2022-03-11 00:12:11 +0000

    selftests/bpf: test subskeleton functionality
    This patch changes the selftests/bpf Makefile to  also generate
    a subskel.h for every skel.h it would have normally generated.
    
    Separately, it also introduces a new subskeleton test which tests
    library objects, externs, weak symbols, kconfigs, and user maps.
    
    Signed-off-by: Delyan Kratunov <delyank@fb.com>

---
+++ tools/testing/selftests/bpf/progs/test_subskeleton.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) Meta Platforms, Inc. and affiliates. */
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+/* volatile to force a read, compiler may assume 0 otherwise */
+const volatile int rovar1;
+int out1;
+
+/* Override weak symbol in test_subskeleton_lib */
+int var5 = 5;
+
+extern int lib_routine(void);
+
+SEC("raw_tp/sys_enter")
+int handler1(const void *ctx)
+{
+	out1 = lib_routine() * rovar1;
+	return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
